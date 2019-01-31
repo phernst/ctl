19e65df1bf9dba26dc11a4c5b26a54a0b760fe08
tpfeiffe <tim.pfeiffer@ovgu.de>
2019-01-31 14:31:39 +0100

    parallelized DetectorSaturationExtension::processExtinctions()

--- modules/src/projectors/detectorsaturationextension.cpp
+++ modules/src/projectors/detectorsaturationextension.cpp
@@ -1,6 +1,8 @@
 #include "detectorsaturationextension.h"
 #include "components/abstractdetector.h"
 
+#include <future>
+
 namespace CTL {
 
 void DetectorSaturationExtension::configure(const AcquisitionSetup &setup, const AbstractProjectorConfig &config)
@@ -26,10 +28,24 @@ void DetectorSaturationExtension::processExtinctions(ProjectionData *projections
 {
     auto saturationModel = _setup.system()->detector()->saturationModel();
 
-    for(auto& view : projections->data())
-        for(auto& module : view.data())
+    auto processView = [saturationModel](SingleViewData* view)
+    {
+        for(auto& module : view->data())
             for(auto& pix : module.data())
                 pix = saturationModel->valueAt(pix);
+    };
+
+    std::vector<std::future<void>> futures;
+    for(auto& view : projections->data())
+        futures.emplace_back(std::async(processView, &view));
+
+//    //sequential
+//    for(auto& view : projections->data())
+//        for(auto& module : view.data())
+//            for(auto& pix : module.data())
+//                pix = saturationModel->valueAt(pix);    // pass pixel value through saturation model
 }
+
+
 
 }
