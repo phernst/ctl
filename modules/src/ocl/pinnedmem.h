2db353710a32aec311508429aeee808705f2d82e
frysch <robert.frysch@ovgu.de>
2019-05-17 15:16:25 +0200

    add pinned memory helper class for cl::Image3D

--- modules/src/ocl/pinnedmem.h
+++ modules/src/ocl/pinnedmem.h
@@ -2,6 +2,7 @@
 #define PINNEDMEM_H
 
 #include "openclconfig.h"
+#include <array>
 
 namespace CTL {
 namespace OCL {
@@ -25,6 +26,25 @@ private:
     cl::Buffer _pinnedBuf;
     cl::Buffer _deviceBuf;
 };
+
+template<typename T, cl_mem_flags devAccess>
+class PinnedMemImag3DBase
+{
+public:
+    PinnedMemImag3DBase(size_t xDim, size_t yDim, size_t zDim);
+
+    const std::array<size_t, 3>& nbElements() const;
+    cl::Image3D& devImage();
+
+protected:
+    cl::Image3D& pinnedImage();
+
+private:
+    std::array<size_t, 3> _nbElements;
+    cl::Image3D _pinnedImg;
+    cl::Image3D _deviceImg;
+};
+
 
 } // namespace _pinnedMemDetails
 
@@ -51,11 +71,29 @@ private:
 };
 
 
-//template<typename T>
-//class PinnedMemImgReadOnly
-//{
-//    PinnedMemImgReadOnly(cl_mem_flags devAccess = CL_MEM_READ_ONLY);
-//};
+template<typename T>
+class PinnedMemImg3DReadOnly : public _pinned_mem_details::PinnedMemImag3DBase<T, CL_MEM_READ_ONLY>
+{
+    PinnedMemImg3DReadOnly(size_t xDim, size_t yDim, size_t zDim, const cl::CommandQueue& queue);
+
+    // access to pinned memory
+    T* hostPtr() const;
+
+    // copy from pinned memory to device (all elements)
+    void copyPinnedMemToDev(bool blocking = true, cl::Event* event = nullptr);
+    // copy from srcPtr to pinned memory (all elements)
+    void copyToPinnedMem(const T* srcPtr);
+
+    // first copy from srcPtr to pinned memory and then to device (all elements)
+    void copyToDev(const T* srcPtr, bool blocking = true, cl::Event* event = nullptr);
+
+private:
+    cl::CommandQueue _q; //!< device specific command queue
+    T* _hostPtr; //!< pointer to pinned memory
+
+    static cl::size_t<3> zeros();
+    cl::size_t<3> dimensions() const;
+};
 
 
 // ### IMPLEMENTATION ###
@@ -92,6 +130,63 @@ void PinnedMemBufReadOnly<T>::copyToDev(const T* srcPtr, bool blocking, cl::Even
     copyPinnedMemToDev(blocking, event);
 }
 
+template<typename T>
+PinnedMemImg3DReadOnly<T>::PinnedMemImg3DReadOnly(size_t xDim, size_t yDim, size_t zDim,
+                                                  const cl::CommandQueue& queue)
+    : _pinned_mem_details::PinnedMemImag3DBase<T, CL_MEM_READ_ONLY>(xDim, yDim, zDim)
+    , _q(queue)
+{
+    size_t rowPitch, slicePitch;
+    _hostPtr = queue.enqueueMapImage(pinnedImage(), CL_TRUE, CL_MAP_WRITE, zeros(), dimensions(),
+                                     rowPitch, slicePitch);
+}
+
+template<typename T>
+T* PinnedMemImg3DReadOnly<T>::hostPtr() const
+{
+    return _hostPtr;
+}
+
+template<typename T>
+void PinnedMemImg3DReadOnly<T>::copyPinnedMemToDev(bool blocking, cl::Event* event)
+{
+    _q.enqueueWriteImage(devImage(), blocking ? CL_TRUE : CL_FALSE, zeros(), dimensions(), 0, 0,
+                         _hostPtr, nullptr, event);
+}
+
+template<typename T>
+void PinnedMemImg3DReadOnly<T>::copyToPinnedMem(const T* srcPtr)
+{
+    std::copy_n(srcPtr, srcPtr + nbElements()[0] * nbElements()[1] * nbElements()[2], _hostPtr);
+}
+
+template<typename T>
+void PinnedMemImg3DReadOnly<T>::copyToDev(const T* srcPtr, bool blocking, cl::Event* event)
+{
+    copyToPinnedMem(srcPtr);
+    copyPinnedMemToDev(blocking, event);
+}
+
+template<typename T>
+cl::size_t<3> PinnedMemImg3DReadOnly<T>::zeros()
+{
+    static cl::size_t<3> zeros;
+    zeros[0] = 0;
+    zeros[1] = 0;
+    zeros[2] = 0;
+    return zeros;
+}
+
+template<typename T>
+cl::size_t<3> PinnedMemImg3DReadOnly<T>::dimensions() const
+{
+    cl::size_t<3> dims;
+    dims[0] = nbElements()[0];
+    dims[1] = nbElements()[1];
+    dims[2] = nbElements()[2];
+    return dims;
+}
+
 
 // ### DETAILS ###
 
@@ -121,6 +216,30 @@ template<typename T, cl_mem_flags devAccess>
 cl::Buffer& PinnedMemBufBase<T, devAccess>::pinnedBuffer()
 {
     return _pinnedBuf;
+}
+
+template<typename T, cl_mem_flags devAccess>
+PinnedMemImag3DBase<T, devAccess>::PinnedMemImag3DBase(size_t xDim, size_t yDim, size_t zDim)
+    : _nbElements{ xDim, yDim, zDim }
+{
+}
+
+template<typename T, cl_mem_flags devAccess>
+const std::array<size_t, 3>& PinnedMemImag3DBase<T, devAccess>::nbElements() const
+{
+    return _nbElements;
+}
+
+template<typename T, cl_mem_flags devAccess>
+cl::Image3D& PinnedMemImag3DBase<T, devAccess>::devImage()
+{
+    return _deviceImg;
+}
+
+template<typename T, cl_mem_flags devAccess>
+cl::Image3D& PinnedMemImag3DBase<T, devAccess>::pinnedImage()
+{
+    return _pinnedImg;
 }
 
 } // namespace _pinned_mem_details
