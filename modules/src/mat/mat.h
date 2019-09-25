26677c73c071e1f489d2732903989068d97ed1e9
frysch <robert.frysch@ovgu.de>
2019-09-25 21:43:27 +0200

    minor improvement of mat.h header

--- modules/src/mat/mat.h
+++ modules/src/mat/mat.h
@@ -1,19 +1,15 @@
 #ifndef MAT_H
 #define MAT_H
 
-// header that includes everything from the `CTL::mat` namespace
-// + Matrix template class (+ MatrixBase + typedefs in "matrix_types.h")
+// This header includes everything from the `CTL::mat` namespace.
+// Plus, it defines a user-defined floating-point literal for conversion from radian to degree.
 
 #include "matrix_algorithm.h"
 #include "pmatcomparator.h"
 
 constexpr long double operator"" _deg(long double deg)
 {
-#ifdef M_PIl
-    return deg * M_PIl / 180.0l;
-#else
-    return deg * static_cast<long double>(M_PI) / 180.0l;
-#endif
+    return deg * 3.141592653589793238462643383279502884l / 180.0l;
 }
 
 #endif // MAT_H
