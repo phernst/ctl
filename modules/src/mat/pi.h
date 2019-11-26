ba2b0fc0150b1dee5d79c224ff5c66d74771ce58
frysch <robert.frysch@ovgu.de>
2019-11-26 10:13:52 +0100

    remove redundant `const`

--- modules/src/mat/pi.h
+++ modules/src/mat/pi.h
@@ -5,9 +5,9 @@
  * Global numeric constants for pi, pi/2 and pi/4.
  */
 
-constexpr const long double PIl = 3.141592653589793238462643383279502884L; // pi
-constexpr const double PI       = 3.14159265358979323846; // pi
-constexpr const double PI_2     = 1.57079632679489661923; // pi/2
-constexpr const double PI_4     = 0.78539816339744830962; // pi/4
+constexpr long double PIl = 3.141592653589793238462643383279502884L; // pi
+constexpr double PI       = 3.14159265358979323846; // pi
+constexpr double PI_2     = 1.57079632679489661923; // pi/2
+constexpr double PI_4     = 0.78539816339744830962; // pi/4
 
 #endif // PI_H
