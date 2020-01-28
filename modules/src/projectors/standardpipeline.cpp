c1210a91a988dede38f9012dfce75ef2703daa03
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-01-28 14:09:09 +0100

    add draft for StandardPipeline

---
+++ modules/src/projectors/standardpipeline.cpp
@@ -0,0 +1,130 @@
+#include "standardpipeline.h"
+
+#include "raycasterprojector.h"
+#include "arealfocalspotextension.h"
+#include "detectorsaturationextension.h"
+#include "poissonnoiseextension.h"
+#include "spectraleffectsextension.h"
+
+namespace CTL {
+
+StandardPipeline::StandardPipeline()
+{
+    _pipeline.setProjector(makeProjector<OCL::RayCasterProjector>());
+
+    _extAFS      = new ArealFocalSpotExtension;
+    _extDetSat   = new DetectorSaturationExtension;
+    _extPoisson  = new PoissonNoiseExtension;
+    _extSpectral = new SpectralEffectsExtension;
+}
+
+StandardPipeline::~StandardPipeline()
+{
+    enableArealFocalSpot(true);
+    enableDetectorSaturation(true);
+    enablePoissonNoise(true);
+    enableSpectralEffects(true);
+}
+
+void StandardPipeline::enableArealFocalSpot(bool enable)
+{
+    if(enable == _arealFSEnabled) // no change
+        return;
+
+    if(enable) // insert AFS into pipeline (first position)
+        _pipeline.insertExtension(posAFS(), _extAFS);
+    else // remove AFS from pipeline (first position)
+        _pipeline.releaseExtension(posAFS());
+
+    _arealFSEnabled = enable;
+}
+
+void StandardPipeline::enableDetectorSaturation(bool enable)
+{
+    if(enable == _detSatEnabled) // no change
+        return;
+
+    if(enable) // insert det. sat. into pipeline (last position)
+        _pipeline.appendExtension(_extDetSat);
+    else // remove det. sat. from pipeline (last position)
+        _pipeline.releaseExtension(_pipeline.nbExtensions());
+
+    _detSatEnabled = enable;
+}
+
+void StandardPipeline::enablePoissonNoise(bool enable)
+{
+    if(enable == _poissonEnabled) // no change
+        return;
+
+    if(enable) // insert Poisson into pipeline (after AFS and spectral)
+        _pipeline.insertExtension(posPoisson(), _extPoisson);
+    else // remove Poisson from pipeline (after AFS and spectral)
+        _pipeline.releaseExtension(posPoisson());
+
+    _poissonEnabled = enable;
+}
+
+void StandardPipeline::enableSpectralEffects(bool enable)
+{
+    if(enable == _spectralEffEnabled) // no change
+        return;
+
+    if(enable) // insert spectral ext. into pipeline (after AFS)
+        _pipeline.insertExtension(posSpectral(), _extSpectral);
+    else // remove Poisson from pipeline (after AFS)
+        _pipeline.releaseExtension(posSpectral());
+
+    _spectralEffEnabled = enable;
+}
+
+void StandardPipeline::setArealFocalSpotDiscretization(const QSize& discretization)
+{
+    _extAFS->setDiscretization(discretization);
+}
+
+void StandardPipeline::setSpectralEffectsSampling(float energyBinWidth)
+{
+    _extSpectral->setSpectralSamplingResolution(energyBinWidth);
+}
+
+uint StandardPipeline::posAFS() const
+{
+    return 0;
+}
+
+uint StandardPipeline::posDetSat() const
+{
+    return uint(_arealFSEnabled)
+            + uint(_spectralEffEnabled)
+            + uint(_poissonEnabled);
+}
+
+uint StandardPipeline::posPoisson() const
+{
+    return uint(_arealFSEnabled)
+            + uint(_spectralEffEnabled);
+}
+
+uint StandardPipeline::posSpectral() const
+{
+    return uint(_arealFSEnabled);
+}
+
+void StandardPipeline::configure(const AcquisitionSetup &setup, const AbstractProjectorConfig &config)
+{
+    _pipeline.configure(setup, config);
+}
+
+ProjectionData StandardPipeline::project(const VolumeData &volume)
+{
+    return _pipeline.project(volume);
+}
+
+bool StandardPipeline::isLinear() const
+{
+    return _pipeline.isLinear();
+}
+
+
+}
---
+++ modules/src/projectors/standardpipeline.h
@@ -0,0 +1,56 @@
+#ifndef STANDARDPIPELINE_H
+#define STANDARDPIPELINE_H
+
+#include "projectionpipeline.h"
+
+namespace CTL {
+
+class ArealFocalSpotExtension;
+class DetectorSaturationExtension;
+class PoissonNoiseExtension;
+class SpectralEffectsExtension;
+
+class StandardPipeline: public AbstractProjector
+{
+public:
+    using ProjectorPtr = std::unique_ptr<AbstractProjector>;
+    using ExtensionPtr = std::unique_ptr<ProjectorExtension>;
+
+    void configure(const AcquisitionSetup& setup,
+                   const AbstractProjectorConfig& config) override;
+    ProjectionData project(const VolumeData& volume) override;
+    bool isLinear() const override;
+
+    StandardPipeline();
+    ~StandardPipeline();
+
+    // configuration methods
+    void enableArealFocalSpot(bool enable);
+    void enableDetectorSaturation(bool enable);
+    void enablePoissonNoise(bool enable);
+    void enableSpectralEffects(bool enable);
+
+    void setArealFocalSpotDiscretization(const QSize& discretization);
+    void setSpectralEffectsSampling(float energyBinWidth);
+
+private:
+    ProjectionPipeline _pipeline;
+
+    bool _arealFSEnabled = false;
+    bool _detSatEnabled = false;
+    bool _spectralEffEnabled = false;
+    bool _poissonEnabled = false;
+
+    ArealFocalSpotExtension* _extAFS;
+    DetectorSaturationExtension* _extDetSat;
+    PoissonNoiseExtension* _extPoisson;
+    SpectralEffectsExtension* _extSpectral;
+
+    uint posAFS() const;
+    uint posDetSat() const;
+    uint posPoisson() const;
+    uint posSpectral() const;
+};
+
+}
+#endif // STANDARDPIPELINE_H
