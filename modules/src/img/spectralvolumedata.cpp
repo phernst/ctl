24d3358070049c442348260a9d9d69988bd4a2da
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-02-20 18:06:24 +0100

    more doc SpectralVolumeData

--- modules/src/img/spectralvolumedata.cpp
+++ modules/src/img/spectralvolumedata.cpp
@@ -53,7 +53,7 @@ SpectralVolumeData::SpectralVolumeData(VoxelVolume<float> muValues,
 
 /*!
  * \brief Constructs a SpectralVolumeData representing the density values \a materialDensity
- * (in g/cm^-3) for a material described by \a absorptionModel.
+ * (in g/cm^3) for a material described by \a absorptionModel.
  *
  *
  */
@@ -126,7 +126,7 @@ SpectralVolumeData SpectralVolumeData::densityVolume() const
  * - a data model describing the spectral dependency of the mass-attenuation coefficient for the
  *   material represented by this volume has been set
  * - one of the following conditions is fulfilled:
- *      * numerical values in individual voxels represent material density (in g/cm^-3), or
+ *      * numerical values in individual voxels represent material density (in g/cm^3), or
  *      * numerical values in individual voxels represent attenuation coefficiens (in 1/mm) and the
  *        corresponding reference energy is specified.
  */
@@ -141,16 +141,28 @@ bool SpectralVolumeData::hasSpectralInformation() const
     return true;
 }
 
+/*!
+ * Returns true if the data stored by this instance are density values (in g/cm^3).
+ */
 bool SpectralVolumeData::isDensityVolume() const
 {
     return !_isMu;
 }
 
+/*!
+ * Returns true if the data stored by this instance are attenuation values (in 1/mm).
+ */
 bool SpectralVolumeData::isMuVolume() const
 {
     return _isMu;
 }
 
+/*!
+ * Returns the mass attenuation coefficient (in cm^2/g) of the material described by this instance
+ * for energy \a atEnergy (in keV).
+ *
+ * Same as absorptionModel()->valueAt(atEnergy).
+ */
 float SpectralVolumeData::massAttenuationCoeff(float atEnergy) const
 {
     if(!_absorptionModel)
@@ -159,11 +171,44 @@ float SpectralVolumeData::massAttenuationCoeff(float atEnergy) const
     return _absorptionModel->valueAt(atEnergy);
 }
 
+/*!
+ * Returns the name of the material described by this instance.
+ */
 const QString& SpectralVolumeData::materialName() const
 {
     return _materialName;
 }
 
+/*!
+ * Returns the attenuation coefficient (with respect to the reference energy \a referenceEnergy)
+ * representation of this instance.
+ *
+ * Note that this creates a copy of the data. The density values are transformed to attenuation
+ * coefficients with respect to the given reference energy \a referenceEnergy. In case this
+ * instance does already contain attenuation coefficient data, the values are re-referenced to
+ * \a referenceEnergy.
+ *
+ * If this instance contains attenuation coefficient data and
+ * \a referenceEnergy == referenceEnergy(), it is discouraged to call this method because it would
+ * only create an unnecessary copy.
+ *
+ * \code
+ *  VoxelVolume<float> muValues(100, 100, 100, 1.0f, 1.0f, 1.0f);
+ *  muValues.fill(0.02f); // 0.02 / mm is approx. attenuation of water for 60 keV
+ *  auto volume = SpectralVolumeData(muValues,
+ *                                   database::attenuationModel(database::Composite::Water),
+ *                                   60.0f);
+ *
+ *  qInfo() << volume.referenceEnergy();                // output: 60
+ *  qInfo() << volume.referenceMassAttenuationCoeff();  // output: 0.2059
+ *
+ *  auto otherVolume = volume.muVolume(60.0f); // unneccesary copy, result identical to 'volume'
+ *  otherVolume = volume.muVolume(45.0f);      // changes reference energy from 60 keV to 45 keV
+ *
+ *  qInfo() << otherVolume.referenceEnergy();                // output: 45
+ *  qInfo() << otherVolume.referenceMassAttenuationCoeff();  // output: 0.2476
+ * \endcode
+ */
 SpectralVolumeData SpectralVolumeData::muVolume(float referenceEnergy) const
 {
     SpectralVolumeData ret(*this);
@@ -176,6 +221,14 @@ SpectralVolumeData SpectralVolumeData::muVolume(float referenceEnergy) const
     return ret;
 }
 
+/*!
+ * Returns the attenuation coefficient with respect to an average mass attenuation coefficient in
+ * the energy interval [\a centerEnergy - \a binWidth / 2, \a centerEnergy + \a binWidth / 2].
+ *
+ * Note that this creates a copy of the data.
+ *
+ * \sa muVolume(float), meanMassAttenuationCoeff().
+ */
 SpectralVolumeData SpectralVolumeData::muVolume(float centerEnergy, float binWidth) const
 {
     SpectralVolumeData ret(*this);
@@ -190,6 +243,10 @@ SpectralVolumeData SpectralVolumeData::muVolume(float centerEnergy, float binWid
     return ret;
 }
 
+/*!
+ * Returns the reference energy corresponding to the attenuation values managed by this instance.
+ * Does not contain meaningful information in case this instance manages density values.
+ */
 float SpectralVolumeData::referenceEnergy() const
 {
     if(!isMuVolume())
@@ -198,6 +255,11 @@ float SpectralVolumeData::referenceEnergy() const
     return _refEnergy;
 }
 
+/*!
+ * Returns the reference mass attenuation coefficient corresponding to the attenuation values
+ * managed by this instance.
+ * Does not contain meaningful information in case this instance manages density values.
+ */
 float SpectralVolumeData::referenceMassAttenuationCoeff() const
 {
     if(!isMuVolume())
@@ -206,6 +268,13 @@ float SpectralVolumeData::referenceMassAttenuationCoeff() const
     return _refMassAttenuationCoeff;
 }
 
+/*!
+ * Creates a SpectralVolumeData object that represents a voxelized ball with radius \a radius,
+ * isometric voxel size \a voxelSize and filled (homogeneuosly) with attenuation value \a muValue.
+ * The voxels surrounding the ball are filled with zeros.
+ *
+ * Note that this creates a volume without spectral information.
+ */
 SpectralVolumeData SpectralVolumeData::createBall(float radius, float voxelSize, float muValue)
 {
     const auto nbVox = static_cast<uint>(std::ceil(2.0f * radius / voxelSize));
@@ -220,6 +289,13 @@ SpectralVolumeData SpectralVolumeData::createBall(float radius, float voxelSize,
     return ret;
 }
 
+/*!
+ * Creates a SpectralVolumeData object that represents a voxelized ball with radius \a radius,
+ * isometric voxel size \a voxelSize (both in mm) and filled (homogeneuosly) with density value
+ * \a density (in g/cm^3). The material properties (i.e. spectrally-dependend mass attenuation
+ * coefficients) are specified by \a absorptionModel. The voxels surrounding the ball are filled
+ * with density 0.0 g/cm^3.
+ */
 SpectralVolumeData
 SpectralVolumeData::createBall(float radius,
                                float voxelSize,
@@ -238,6 +314,12 @@ SpectralVolumeData::createBall(float radius,
     return ret;
 }
 
+/*!
+ * Replaces the absorption model in this instance by \a absorptionModel (must not be nullptr).
+ *
+ * Note that this is only allowed for objects that already have spectral information available and
+ * throws an std::runtime_error otherwise.
+ */
 void SpectralVolumeData::replaceAbsorptionModel(AbstractIntegrableDataModel* absorptionModel)
 {
     if(!hasSpectralInformation())
@@ -250,6 +332,12 @@ void SpectralVolumeData::replaceAbsorptionModel(AbstractIntegrableDataModel* abs
     _absorptionModel.reset(absorptionModel);
 }
 
+/*!
+ * Replaces the absorption model in this instance by \a absorptionModel (must not be nullptr).
+ *
+ * Note that this is only allowed for objects that already have spectral information available and
+ * throws an std::runtime_error otherwise.
+ */
 void SpectralVolumeData::replaceAbsorptionModel(std::shared_ptr<AbstractIntegrableDataModel> absorptionModel)
 {
     if(!hasSpectralInformation())
@@ -262,9 +350,15 @@ void SpectralVolumeData::replaceAbsorptionModel(std::shared_ptr<AbstractIntegrab
     _absorptionModel = std::move(absorptionModel);
 }
 
+/*!
+ * Replaces the voxel data in this instance by the density values given by \a density (in g/cm^3).
+ *
+ * Note that this is only allowed for objects that have a non-defaulted absorbtion model set and
+ * throws an std::runtime_error otherwise.
+ */
 void SpectralVolumeData::setDensity(VoxelVolume<float> density)
 {
-    if(!_absorptionModel)
+    if(!_hasNonDefaultAbsModel)
         throw std::runtime_error("SpectralVolumeData::setDensity: Cannot set density values: No "
                                  "absorption model set!");
 
@@ -272,11 +366,21 @@ void SpectralVolumeData::setDensity(VoxelVolume<float> density)
     _isMu = false;
 }
 
+/*!
+ * Sets the name of the material described by this instance to \a name.
+ */
 void SpectralVolumeData::setMaterialName(const QString& name)
 {
     _materialName = name;
 }
 
+/*!
+ * Creates a SpectralVolumeData object from the attenuation values given by \a muValues (in 1 / mm)
+ * corresponding to the reference energy \a referenceEnergy of the material specified by its
+ * spectrally-dependend mass attenuation coefficients in \a absorptionModel.
+ *
+ * Generates the density representation of the data.
+ */
 SpectralVolumeData SpectralVolumeData::fromMuVolume(VoxelVolume<float> muValues,
                                                     std::shared_ptr<AbstractIntegrableDataModel> absorptionModel,
                                                     float referenceEnergy)
@@ -293,6 +397,13 @@ SpectralVolumeData SpectralVolumeData::fromMuVolume(VoxelVolume<float> muValues,
     return SpectralVolumeData(std::move(muValues), absorptionModel, absorptionModel->name());
 }
 
+/*!
+ * Creates a SpectralVolumeData object from the attenuation values given by \a muValues (in
+ * Hounsfield units) corresponding to the reference energy \a referenceEnergy of the material
+ * specified by its spectrally-dependend mass attenuation coefficients in \a absorptionModel.
+ *
+ * Generates the density representation of the data.
+ */
 SpectralVolumeData SpectralVolumeData::fromHUVolume(VoxelVolume<float> HUValues,
                                                     std::shared_ptr<AbstractIntegrableDataModel> absorptionModel,
                                                     float referenceEnergy)
@@ -313,6 +424,10 @@ SpectralVolumeData SpectralVolumeData::fromHUVolume(VoxelVolume<float> HUValues,
 
     return SpectralVolumeData(std::move(HUValues), absorptionModel, absorptionModel->name());
 }
+
+// ###############
+// private methods
+// ###############
 
 void SpectralVolumeData::changeReferenceEnergy(float newReferenceEnergy)
 {
