000a03c234388ee644da258b8da0c91c3098994f
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-03-06 16:48:24 +0100

    add code example to doc of PoissonNoiseExtension

--- modules/src/projectors/poissonnoiseextension.h
+++ modules/src/projectors/poissonnoiseextension.h
@@ -21,9 +21,47 @@ namespace CTL {
  *
  * Poisson-distributed random numbers are generated using the Mersenne twister engine
  * (std::mt19937). A fixed seed can be used to create reproducible results (see setFixedSeed()).
- * \code
  *
+ * The following code example shows how to extend a simple ray caster algorithm to add Poisson noise
+ * to the simulated projections:
+ * \code
+ * // define volume and acquisition setup (incl. system)
+ * auto volume = VolumeData::cube(100, 1.0f, 0.02f);
+ * auto system = SimpleCTsystem::fromCTsystem(CTsystemBuilder::createFromBlueprint(blueprints::GenericCarmCT(DetectorBinning::Binning4x4)));
+ *
+ * // reduce default radiation output of the source component (-> make noise more prominent)
+ * static_cast<XrayTube*>(system.source())->setMilliampereSeconds(0.001);
+ *
+ * AcquisitionSetup acquisitionSetup(system, 10);
+ * acquisitionSetup.applyPreparationProtocol(protocols::ShortScanTrajectory(750.0));
+ *
+ * // Core part
+ * auto simpleProjector = new RayCasterProjector; // our simple projector
+ *     // optional parameter settings for the projector
+ *     // e.g. simpleProjector->settings().raySampling = 0.1f;
+ *
+ * // this is what you do without extension (i.e. noise-free):
+ *     // simpleProjector->configure(acquisitionSetup);
+ *     // ProjectionData projections = simpleProjector->project(volume);
+ *
+ * // instead we now do the following
+ * PoissonNoiseExtension* extension = new PoissonNoiseExtension;
+ *
+ * extension->use(simpleProjector);                            // tell the extension to use the ray caster
+ * extension->setFixedSeed(42);                                // setting a fixed seed for random number generation
+ * extension->configure(acquisitionSetup);                     // configure the simulation
+ *
+ * ProjectionData projections = extension->project(volume);    // (compute and) get the final (noisy) projections
+ *
+ * // visualization (optional, requires 'ctl_qtgui' submodule):
+ *     // auto viewer = new ProjectionView();
+ *     // viewer->resize(800, 600);
+ *     // viewer->show();
+ *     // viewer->setData(projections);
  * \endcode
+ *
+ * The difference between the projections with and without noise in the example above is shown here:
+ * ![Simulated projections of a homogeneous water cube without (left) and with Poisson noise (right).](PoissonNoiseExtension.png)
  */
 class PoissonNoiseExtension : public ProjectorExtension
 {
