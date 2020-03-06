867525c6525765e19b4bb42dafda43ebe9f9fb74
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-03-06 14:39:16 +0100

    add code example to DetectorSaturationExtension doc

--- modules/src/projectors/detectorsaturationextension.h
+++ modules/src/projectors/detectorsaturationextension.h
@@ -18,10 +18,39 @@ namespace CTL {
  * saturation model, the postprocessing will be applied in the domain of extinction values,
  * intensities, or photon counts.
  * 
- * The following example shows how to extend a simple ray caster algorithm to consider a detector
- * saturation extension:
+ * The following example shows how to extend a simple ray caster algorithm to consider detector
+ * (over- and under-) saturation:
  * \code
+ *   // define volume and acquisition setup (incl. system)
+ *  auto volume = VolumeData::cube(100, 1.0f, 0.02f);
+ *  auto system = SimpleCTsystem::fromCTsystem(CTsystemBuilder::createFromBlueprint(blueprints::GenericCarmCT(DetectorBinning::Binning4x4)));
+ *  AcquisitionSetup acquisitionSetup(system, 10);
+ *  acquisitionSetup.applyPreparationProtocol(protocols::ShortScanTrajectory(750.0));
  *
+ *  // Core part
+ *
+ *  // set a detector saturation model (operating in extinction domain, clamps values to [0.1, 2.5])
+ *  auto saturationModel = new DetectorSaturationLinearModel(0.1f, 2.5f);
+ *  acquisitionSetup.system()->detector()->setSaturationModel(saturationModel, AbstractDetector::Extinction);
+ *
+ *  auto simpleProjector = new RayCasterProjector; // our simple projector
+ *      // optional parameter settings for the projector
+ *      // e.g. simpleProjector->settings().raySampling = 0.1f;
+ *
+ *  // This is what we do without the extension:
+ *      // simpleProjector->configure(acquisitionSetup);
+ *      // ProjectionData projections = simpleProjector->project(volume);
+ *      // qInfo() << projections.min() << projections.max(); // output: 0 2.79263
+ *
+ *  // Instead we now do the following:
+ *  DetectorSaturationExtension* extension = new DetectorSaturationExtension;
+ *
+ *  extension->use(simpleProjector);                            // tell the extension to use the ray caster
+ *  extension->configure(acquisitionSetup);                     // configure the simulation
+ *
+ *  ProjectionData projections = extension->project(volume);    // (compute and) get the final projections
+ *
+ *  qInfo() << projections.min() << projections.max();          // output: 0.1 2.5
  * \endcode
  */
 
