643de81dd8ff4e97ec1e6bfd01218ae5c5525aef
timpf <timpf@DESKTOP-8CCTD3U.local>
2020-03-17 16:15:02 +0100

    finish documentation of CompositeVolume, incl. many code examples

--- modules/src/img/compositevolume.cpp
+++ modules/src/img/compositevolume.cpp
@@ -69,6 +69,17 @@ bool CompositeVolume::isEmpty() const
 
 /*!
  * Adds \a volume as aub-volume to this instance.
+ *
+ * Example:
+ * \code
+ * CompositeVolume volume;
+ *
+ * // add a water cube
+ * volume.addSubVolume(SpectralVolumeData::cube(50, 1.0f, 1.0f, attenuationModel(database::Composite::Water)));
+ *
+ * // we can also add a plain VoxelVolume (i.e. representing attenuation coefficients)
+ * volume.addSubVolume(VoxelVolume<float>::ball(20.0f, 1.0f, 0.05f)); // this uses implicit cast to SpectralVolumeData
+ * \endcode
  */
 void CompositeVolume::addSubVolume(SpectralVolumeData volume)
 {
@@ -85,6 +96,17 @@ void CompositeVolume::addSubVolume(std::unique_ptr<SpectralVolumeData> volume)
 
 /*!
  * Adds \a volume as aub-volume to this instance. The volume will be cloned.
+ *
+ * Example:
+ * \code
+ * CompositeVolume volume;
+ *
+ * // define a simple dynamic volume: cube that increases in attenuation by 0.01/mm per millisecond
+ * LinearDynamicVolume dynamicVol(0.01f, 0.0f, { 100, 100, 100 }, { 1.0f, 1.0f, 1.0f });
+ *
+ * // add it as sub-volume
+ * volume.addSubVolume(dynamicVol);
+ * \endcode
  */
 void CompositeVolume::addSubVolume(const AbstractDynamicVolumeData& volume)
 {
@@ -93,6 +115,22 @@ void CompositeVolume::addSubVolume(const AbstractDynamicVolumeData& volume)
 
 /*!
  * Adds all sub-volumes of \a volume to this instance.
+ *
+ * Example:
+ * \code
+ * CompositeVolume volume;  // our final volume
+ *
+ * // We first add a water cube to the volume
+ * volume.addSubVolume(SpectralVolumeData::cube(100, 1.0f, 1.0f, attenuationModel(database::Composite::Water)));
+ *
+ * // We now add another composite, consisting of two testicle balls.
+ * volume.addSubVolume(CompositeVolume(SpectralVolumeData::ball(15.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Testis)),
+ *                                     SpectralVolumeData::ball(15.0f, 1.0f, 1.1f, attenuationModel(database::Composite::Testis))));
+ *
+ * // If necessary, we can now shift individual sub-volumes slightly around the center, e.g. like that:
+ * volume.subVolume(1).setVolumeOffset(-20.0f, 0.0f, 0.0f); // shifts ball 1 towards a negative x position
+ * volume.subVolume(2).setVolumeOffset( 20.0f, 0.0f, 0.0f); // shifts ball 2 towards a positive x position
+ * \endcode
  */
 void CompositeVolume::addSubVolume(CompositeVolume&& volume)
 {
@@ -104,6 +142,27 @@ void CompositeVolume::addSubVolume(CompositeVolume&& volume)
 
 /*!
  * Adds all sub-volumes of \a volume to this instance. All sub-volumes will be cloned.
+ *
+ * Example:
+ * \code
+ * CompositeVolume volume;  // our final volume
+ *
+ * // We first add a water cube to the volume
+ * volume.addSubVolume(SpectralVolumeData::cube(100, 1.0f, 1.0f, attenuationModel(database::Composite::Water)));
+ *
+ * // We now construct another composite, consisting of two testicle balls.
+ * CompositeVolume subComposite {
+ *     SpectralVolumeData::ball(15.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Testis)),
+ *     SpectralVolumeData::ball(15.0f, 1.0f, 1.1f, attenuationModel(database::Composite::Testis))
+ * };
+ *
+ * // We shift both sub-volumes slightly around the center.
+ * subComposite.subVolume(0).setVolumeOffset(-20.0f, 0.0f, 0.0f);
+ * subComposite.subVolume(1).setVolumeOffset( 20.0f, 0.0f, 0.0f);
+ *
+ * // Now, we can add the composite containing the two balls to our final volume (which already holds the water cube).
+ * volume.addSubVolume(subComposite);
+ * \endcode
  */
 void CompositeVolume::addSubVolume(const CompositeVolume& volume)
 {
--- modules/src/img/compositevolume.h
+++ modules/src/img/compositevolume.h
@@ -34,6 +34,77 @@ namespace CTL {
  * - (positional) offset
  * - volume type (plain, spectral, dynamic)
  * This allows for fully flexible composition of phantom data.
+ *
+ * The following code example shows one possibility to construct a volume consisting of a cube
+ * filled with extinction value 0.02/mm and two balls, a smaller one with 0.05/mm and a slightly
+ * larger one with 0.1/mm. We want the first ball to be positioned slightly in negative y-direction and
+ * the second one in the opposite direction.
+ * \code
+ * // Construct the composite volume already containing the cube volume.
+ * CompositeVolume volume(VoxelVolume<float>::cube(150, 1.0f, 0.02f));
+ *
+ * // We now construct the two ball volumes.
+ * auto subVolume1 = VoxelVolume<float>::ball(10.0f, 1.0f, 0.05f);
+ * auto subVolume2 = VoxelVolume<float>::ball(25.0f, 1.0f, 0.10f);
+ *
+ * // Here, we shift the ball volumes to the desired positions.
+ * subVolume1.setVolumeOffset(0.0f, -20.0f, 0.0f);
+ * subVolume2.setVolumeOffset(0.0f,  30.0f, 0.0f);
+ *
+ * // Now, we add the two balls as sub-volumes to our final volume.
+ * volume.addSubVolume(std::move(subVolume1));
+ * volume.addSubVolume(std::move(subVolume2));
+ * \endcode
+ *
+ * We can now create a projection image from this composite with the following example code:
+ * \code
+ * // First, we need to define an acquisition setup (with a CT system and the number of views; 10 in this case)
+ * AcquisitionSetup setup(CTsystemBuilder::createFromBlueprint(blueprints::GenericCarmCT(DetectorBinning::Binning4x4)), 10);
+ * // We also need to specify the acquisition geometry, here we set a simple short scan trajectory
+ * setup.applyPreparationProtocol(protocols::ShortScanTrajectory(750.0f));
+ *
+ * // Now, we create our projector, here we simply use the standard pipeline.
+ * auto projector = makeProjector<StandardPipeline>();
+ *
+ * // Pass the acquisition setup to the projector and create the projections:
+ * projector->configure(setup);
+ * auto projections = projector->projectComposite(volume);
+ * \endcode
+ *
+ * ![First view of the projections generated with the above example.](compositeVolume.png)
+ *
+ * The same can be done with spectral volumes as well. It is also possible to mix both types as
+ * shown in the following example. Note, however, that these mixtures are currently (v. 0.3.1)
+ * only supported in the non-linear case of SpectralEffectsExtension (which is used for example
+ * with the StandardPipeline in No_Approximation preset).
+ * \code
+ * // Our starting volume remains the same. This is the sub-volume without spectral information.
+ * CompositeVolume volume(VoxelVolume<float>::cube(150, 1.0f, 0.02f));
+ *
+ * // We now create two balls with spectral information (one representing blood, the other bone).
+ * auto subVolume1 = SpectralVolumeData::ball(10.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Blood));
+ * auto subVolume2 = SpectralVolumeData::ball(25.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Bone_Cortical));
+ *
+ * // Again, the shift to the desired positions...
+ * subVolume1.setVolumeOffset(0.0f, -20.0f, 0.0f);
+ * subVolume2.setVolumeOffset(0.0f,  30.0f, 0.0f);
+ *
+ * // ... and adding to the final volume.
+ * volume.addSubVolume(std::move(subVolume1));
+ * volume.addSubVolume(std::move(subVolume2));
+ *
+ * // In the projection code, we only change the setting for the standard pipeline to 'No_Approximation'...
+ * AcquisitionSetup setup(CTsystemBuilder::createFromBlueprint(blueprints::GenericCarmCT(DetectorBinning::Binning4x4)), 10);
+ * setup.applyPreparationProtocol(protocols::ShortScanTrajectory(750.0f));
+ *
+ * //... here comes the changed line:
+ * auto projector = makeProjector<StandardPipeline>(StandardPipeline::No_Approximation);
+ *
+ * projector->configure(setup);
+ * auto projections = projector->projectComposite(volume);
+ * \endcode
+ *
+ * ![First view of the projections generated with the example for mixed volumes.](compositeVolume_mixed.png)
  */
 class CompositeVolume
 {
@@ -81,6 +152,15 @@ private:
 
 /*!
  * Constructs a CompositeVolume and adds all data passed to the constructor as sub-volumes.
+ *
+ * Example:
+ * \code
+ * // pass a water cube
+ * CompositeVolume volume(SpectralVolumeData::cube(50, 1.0f, 1.0f, attenuationModel(database::Composite::Water)));
+ *
+ * // we can also pass a plain VoxelVolume (i.e. representing attenuation coefficients)
+ * CompositeVolume volume2(VoxelVolume<float>::ball(20.0f, 1.0f, 0.05f)); // this uses implicit cast to SpectralVolumeData
+ * \endcode
  */
 template <class... Volumes>
 CompositeVolume::CompositeVolume(SpectralVolumeData volume, Volumes&&... otherVolumes)
@@ -102,6 +182,12 @@ CompositeVolume::CompositeVolume(std::unique_ptr<SpectralVolumeData> volume,
 
 /*!
  * Constructs a CompositeVolume and adds all data passed to the constructor as sub-volumes.
+ *
+ * Example:
+ * \code
+ * // pass a simple dynamic volume: cube that increases in attenuation by 0.01/mm per millisecond
+ * CompositeVolume volume(LinearDynamicVolume(0.01f, 0.0f, { 100, 100, 100 }, { 1.0f, 1.0f, 1.0f }));
+ * \endcode
  */
 template <class... Volumes>
 CompositeVolume::CompositeVolume(const AbstractDynamicVolumeData& volume, Volumes&&... otherVolumes)
@@ -112,6 +198,16 @@ CompositeVolume::CompositeVolume(const AbstractDynamicVolumeData& volume, Volume
 
 /*!
  * Constructs a CompositeVolume and adds all data passed to the constructor as sub-volumes.
+ *
+ * Example:
+ * \code
+ * // Passing a composite consisting of two balls as well as a single cube volume:
+ * CompositeVolume volume(CompositeVolume {
+ *                            SpectralVolumeData::ball(15.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Blood)),
+ *                            SpectralVolumeData::ball(15.0f, 1.0f, 1.1f, attenuationModel(database::Composite::Blood))
+ *                        },
+ *                        SpectralVolumeData::cube(100, 1.0f, 1.0f, attenuationModel(database::Composite::Water)));
+ * \endcode
  */
 template <class... Volumes>
 CompositeVolume::CompositeVolume(CompositeVolume&& volume, Volumes&&... otherVolumes)
@@ -125,6 +221,16 @@ CompositeVolume::CompositeVolume(CompositeVolume&& volume, Volumes&&... otherVol
 
 /*!
  * Constructs a CompositeVolume and adds all data passed to the constructor as sub-volumes.
+ *
+ * Example:
+ * \code
+ * // Passing a composite consisting of two balls as well as a single cube volume:
+ * CompositeVolume volume(CompositeVolume {
+ *                            SpectralVolumeData::ball(15.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Blood)),
+ *                            SpectralVolumeData::ball(15.0f, 1.0f, 1.1f, attenuationModel(database::Composite::Blood))
+ *                        },
+ *                        SpectralVolumeData::cube(100, 1.0f, 1.0f, attenuationModel(database::Composite::Water)));
+ * \endcode
  */
 template <class... Volumes>
 CompositeVolume::CompositeVolume(const CompositeVolume& volume, Volumes&&... otherVolumes)
