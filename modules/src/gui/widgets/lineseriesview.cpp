84149f95003634c6c6dae8a62b4b07ec88cf9177
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-03-26 16:32:49 +0100

    refactored LineSeriesView

--- modules/src/gui/widgets/lineseriesview.cpp
+++ modules/src/gui/widgets/lineseriesview.cpp
@@ -1,34 +1,66 @@
 #include "lineseriesview.h"
 
+#include "models/xydataseries.h"
+
 namespace CTL {
 namespace gui {
 
 LineSeriesView::LineSeriesView(QWidget* parent)
     : QChartView(parent)
+    , _data(new QLineSeries)
+    , _chart(new QChart)
 {
     setWindowTitle("Line Series View");
+
+    setChart(_chart);
+    _chart->addSeries(_data);
+    _chart->legend()->hide();
+    _chart->createDefaultAxes();
+
+    setRubberBand(QChartView::RectangleRubberBand);
 }
 
-void LineSeriesView::plot(const QList<QPointF>& lineSeries,
+void LineSeriesView::plot(const XYDataSeries& lineSeries,
                           const QString& labelX, const QString& labelY)
 {
     auto viewer = new LineSeriesView;
     viewer->setAttribute(Qt::WA_DeleteOnClose);
 
-    auto chart = new QChart;
+    viewer->setData(lineSeries);
 
-    auto series = new QLineSeries;
-    series->append(lineSeries);
+    viewer->_chart->axisX()->setTitleText(labelX);
+    viewer->_chart->axisY()->setTitleText(labelY);
 
-    chart->legend()->hide();
-    chart->addSeries(series);
-    chart->createDefaultAxes();
-    chart->axisX()->setTitleText(labelX);
-    chart->axisY()->setTitleText(labelY);
-
-    viewer->setChart(chart);
     viewer->resize(500,400);
     viewer->show();
+}
+
+void LineSeriesView::plot(const QList<QPointF>& lineSeries,
+                          const QString& labelX, const QString& labelY)
+{
+    plot(XYDataSeries(lineSeries), labelX, labelY);
+}
+
+void LineSeriesView::setData(const XYDataSeries& lineSeries)
+{
+    _data->clear();
+    _data->append(lineSeries.data());
+
+    const auto samplingPts = lineSeries.samplingPoints();
+    QPair<float, float> xRange(*std::min_element(samplingPts.cbegin(), samplingPts.cend()),
+                               *std::max_element(samplingPts.cbegin(), samplingPts.cend()));
+    _chart->axisX(_data)->setRange(xRange.first, xRange.second);
+    _chart->axisY(_data)->setRange(lineSeries.min(), 1.05 * lineSeries.max());
+}
+
+void LineSeriesView::setData(const QList<QPointF>& lineSeries)
+{
+    setData(XYDataSeries(lineSeries));
+}
+
+void LineSeriesView::setShowPoints(bool enabled)
+{
+    _data->setPointsVisible(enabled);
 }
 
 } // namespace gui
--- modules/src/gui/widgets/lineseriesview.h
+++ modules/src/gui/widgets/lineseriesview.h
@@ -4,6 +4,9 @@
 #include <QtCharts>
 
 namespace CTL {
+
+class XYDataSeries;
+
 namespace gui {
 
 class LineSeriesView : public QChartView
@@ -12,10 +15,22 @@ class LineSeriesView : public QChartView
 public:
     explicit LineSeriesView(QWidget *parent = nullptr);
 
+    static void plot(const XYDataSeries& lineSeries,
+                     const QString& labelX = "x",
+                     const QString& labelY = "y");
     static void plot(const QList<QPointF>& lineSeries,
                      const QString& labelX = "x",
                      const QString& labelY = "y");
 
+    void setData(const XYDataSeries& lineSeries);
+    void setData(const QList<QPointF>& lineSeries);
+
+public slots:
+    void setShowPoints(bool enabled = true);
+
+private:
+    QLineSeries* _data;
+    QChart* _chart;
 };
 
 } // namespace gui
