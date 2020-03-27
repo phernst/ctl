2aac86473bf65b7ef99341dcc4765b05ea0a4beb
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-03-27 12:06:21 +0100

    refactored LineSeriesView, add option to use logarithmic y-axis

--- modules/src/gui/widgets/lineseriesview.cpp
+++ modules/src/gui/widgets/lineseriesview.cpp
@@ -7,50 +7,59 @@ namespace gui {
 
 LineSeriesView::LineSeriesView(QWidget* parent)
     : QChartView(parent)
-    , _data(new QLineSeries)
+    , _dataSeries(new QLineSeries)
+    , _dataSeriesLog(new QLineSeries)
     , _chart(new QChart)
 {
     setWindowTitle("Line Series View");
 
     setChart(_chart);
-    _chart->addSeries(_data);
+    _chart->addSeries(_dataSeries);
+    _chart->addSeries(_dataSeriesLog);
+    _chart->setAxisX(new QValueAxis, _dataSeries);
+    _chart->setAxisY(new QValueAxis, _dataSeries);
+    _chart->setAxisX(new QValueAxis, _dataSeriesLog);
+    _chart->setAxisY(new QLogValueAxis, _dataSeriesLog);
+
+    setSeriesShow(_dataSeriesLog, false);
+
     _chart->legend()->hide();
-    _chart->createDefaultAxes();
 
     setRubberBand(QChartView::RectangleRubberBand);
 }
 
 void LineSeriesView::plot(const XYDataSeries& lineSeries,
-                          const QString& labelX, const QString& labelY)
+                          const QString& labelX, const QString& labelY, bool logAxisY)
 {
     auto viewer = new LineSeriesView;
     viewer->setAttribute(Qt::WA_DeleteOnClose);
 
     viewer->setData(lineSeries);
 
-    viewer->_chart->axisX()->setTitleText(labelX);
-    viewer->_chart->axisY()->setTitleText(labelY);
+    viewer->setLabelX(labelX);
+    viewer->setLabelY(labelY);
+
+    viewer->setLogAxisY(logAxisY);
 
     viewer->resize(500,400);
     viewer->show();
 }
 
 void LineSeriesView::plot(const QList<QPointF>& lineSeries,
-                          const QString& labelX, const QString& labelY)
+                          const QString& labelX, const QString& labelY, bool logAxisY)
 {
-    plot(XYDataSeries(lineSeries), labelX, labelY);
+    plot(XYDataSeries(lineSeries), labelX, labelY, logAxisY);
 }
 
 void LineSeriesView::setData(const XYDataSeries& lineSeries)
 {
-    _data->clear();
-    _data->append(lineSeries.data());
+    _dataSeries->clear();
+    _dataSeries->append(lineSeries.data());
 
-    const auto samplingPts = lineSeries.samplingPoints();
-    QPair<float, float> xRange(*std::min_element(samplingPts.cbegin(), samplingPts.cend()),
-                               *std::max_element(samplingPts.cbegin(), samplingPts.cend()));
-    _chart->axisX(_data)->setRange(xRange.first, xRange.second);
-    _chart->axisY(_data)->setRange(lineSeries.min(), 1.05 * lineSeries.max());
+    if(!yAxisIsLinear())
+        updateLogData();
+
+    autoRange();
 }
 
 void LineSeriesView::setData(const QList<QPointF>& lineSeries)
@@ -58,32 +67,133 @@ void LineSeriesView::setData(const QList<QPointF>& lineSeries)
     setData(XYDataSeries(lineSeries));
 }
 
-void LineSeriesView::autoZoom() const
+void LineSeriesView::autoRange() const
 {
     auto compareX = [] (const QPointF& a, const QPointF& b) { return a.x()<b.x(); };
     auto compareY = [] (const QPointF& a, const QPointF& b) { return a.y()<b.y(); };
 
-    const auto dataPts = _data->pointsVector();
-    QPair<double, double> xRange(std::min_element(dataPts.cbegin(), dataPts.cend(), compareX)->x(),
-                                 std::max_element(dataPts.cbegin(), dataPts.cend(), compareX)->x());
-    QPair<double, double> yRange(std::min_element(dataPts.cbegin(), dataPts.cend(), compareY)->y(),
-                                 std::max_element(dataPts.cbegin(), dataPts.cend(), compareY)->y());
+    const auto dataPts = yAxisIsLinear() ? _dataSeries->pointsVector()
+                                         : _dataSeriesLog->pointsVector();
 
-    _chart->axisX(_data)->setRange(xRange.first, xRange.second);
-    _chart->axisY(_data)->setRange(yRange.first, 1.05 * yRange.second);
+    const auto minMaxX = std::minmax_element(dataPts.cbegin(), dataPts.cend(), compareX);
+    const auto minMaxY = std::minmax_element(dataPts.cbegin(), dataPts.cend(), compareY);
+
+    const auto xRange = qMakePair(minMaxX.first->x(), minMaxX.second->x());
+    const auto yRange = qMakePair(minMaxY.first->y(), minMaxY.second->y());
+
+    if(yAxisIsLinear())
+    {
+        _chart->axisX(_dataSeries)->setRange(xRange.first, xRange.second);
+        _chart->axisY(_dataSeries)->setRange(yRange.first, 1.05 * yRange.second);
+        if(_useNiceX)
+            qobject_cast<QValueAxis*>(_chart->axisX(_dataSeries))->applyNiceNumbers();
+    }
+    else
+    {
+        _chart->axisX(_dataSeriesLog)->setRange(xRange.first, xRange.second);
+        _chart->axisY(_dataSeriesLog)->setRange(yRange.first, 1.05 * yRange.second);
+        if(_useNiceX)
+            qobject_cast<QValueAxis*>(_chart->axisX(_dataSeriesLog))->applyNiceNumbers();
+    }
+}
+
+void LineSeriesView::setLabelX(const QString& label)
+{
+    _chart->axisX(_dataSeries)->setTitleText(label);
+    _chart->axisX(_dataSeriesLog)->setTitleText(label);
+}
+
+void LineSeriesView::setLabelY(const QString& label)
+{
+    _chart->axisY(_dataSeries)->setTitleText(label);
+    _chart->axisY(_dataSeriesLog)->setTitleText(label);
 }
 
 void LineSeriesView::setShowPoints(bool enabled)
 {
-    _data->setPointsVisible(enabled);
+    _dataSeries->setPointsVisible(enabled);
+}
+
+void LineSeriesView::setLogAxisY(bool enabled)
+{
+    enabled ? switchToLogAxisY() : switchToLinAxisY();
+}
+
+void LineSeriesView::setUseNiceX(bool enabled) { _useNiceX = enabled; }
+
+void LineSeriesView::toggleLinLogY()
+{
+    if(yAxisIsLinear())
+        switchToLogAxisY();
+    else
+        switchToLinAxisY();
 }
 
 void LineSeriesView::mouseDoubleClickEvent(QMouseEvent *event)
 {
     if(event->button() == Qt::LeftButton)
-        autoZoom();
+        autoRange();
 
     event->accept();
+}
+
+bool LineSeriesView::yAxisIsLinear() const { return _dataSeries->isVisible(); }
+
+void LineSeriesView::switchToLinAxisY()
+{
+    const auto logAxisX = qobject_cast<QValueAxis*>(_chart->axisX(_dataSeriesLog));
+    const auto logAxisY = qobject_cast<QLogValueAxis*>(_chart->axisY(_dataSeriesLog));
+
+    const auto rangeX = qMakePair(logAxisX->min(), logAxisX->max());
+    const auto rangeY = qMakePair(logAxisY->min(), logAxisY->max());
+
+    setSeriesShow(_dataSeriesLog, false);
+    setSeriesShow(_dataSeries, true);
+
+    _chart->axisX(_dataSeries)->setRange(rangeX.first, rangeX.second);
+    _chart->axisY(_dataSeries)->setRange(rangeY.first, rangeY.second);
+}
+
+void LineSeriesView::switchToLogAxisY()
+{
+    const auto linAxisX = qobject_cast<QValueAxis*>(_chart->axisX(_dataSeries));
+    const auto linAxisY = qobject_cast<QValueAxis*>(_chart->axisY(_dataSeries));
+
+    const auto rangeX = qMakePair(linAxisX->min(), linAxisX->max());
+    const auto rangeY = qMakePair(linAxisY->min(), linAxisY->max());
+
+    updateLogData();
+
+    setSeriesShow(_dataSeries, false);
+    setSeriesShow(_dataSeriesLog, true);
+
+    _chart->axisX(_dataSeriesLog)->setRange(rangeX.first, rangeX.second);
+    _chart->axisY(_dataSeriesLog)->setRange(std::max( { rangeY.first,  0.01 } ),
+                                         std::max( { rangeY.second, 0.01 } ));
+}
+
+void LineSeriesView::updateLogData()
+{
+    _dataSeriesLog->clear();
+    for(const auto& pt : _dataSeries->pointsVector())
+        if(pt.y() > 0.0)
+            _dataSeriesLog->append(pt);
+}
+
+void LineSeriesView::setSeriesShow(QAbstractSeries* series, bool shown)
+{
+    if(shown)
+    {
+        series->show();
+        _chart->axisX(series)->show();
+        _chart->axisY(series)->show();
+    }
+    else
+    {
+        series->hide();
+        _chart->axisX(series)->hide();
+        _chart->axisY(series)->hide();
+    }
 }
 
 } // namespace gui
--- modules/src/gui/widgets/lineseriesview.h
+++ modules/src/gui/widgets/lineseriesview.h
@@ -17,24 +17,39 @@ public:
 
     static void plot(const XYDataSeries& lineSeries,
                      const QString& labelX = "x",
-                     const QString& labelY = "y");
+                     const QString& labelY = "y",
+                     bool logAxisY = false);
     static void plot(const QList<QPointF>& lineSeries,
                      const QString& labelX = "x",
-                     const QString& labelY = "y");
+                     const QString& labelY = "y",
+                     bool logAxisY = false);
 
     void setData(const XYDataSeries& lineSeries);
     void setData(const QList<QPointF>& lineSeries);
 
 public slots:
-    void autoZoom() const;
+    void autoRange() const;
+    void setLabelX(const QString& label);
+    void setLabelY(const QString& label);
+    void setLogAxisY(bool enabled);
+    void setUseNiceX(bool enabled);
     void setShowPoints(bool enabled = true);
+    void toggleLinLogY();
 
 protected:
     void mouseDoubleClickEvent(QMouseEvent *event) override;
 
 private:
-    QLineSeries* _data;
+    QLineSeries* _dataSeries;
+    QLineSeries* _dataSeriesLog;
     QChart* _chart;
+    bool _useNiceX = false;
+
+    void setSeriesShow(QAbstractSeries* series, bool shown);
+    void switchToLinAxisY();
+    void switchToLogAxisY();
+    void updateLogData();
+    bool yAxisIsLinear() const;
 };
 
 } // namespace gui
