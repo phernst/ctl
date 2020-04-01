2eeaf99735b1a8df3af20473d8248dd0851a8f2a
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-04-01 11:43:42 +0200

    WindowingWidget: removed legacy code

--- modules/src/gui/widgets/windowingwidget.h
+++ modules/src/gui/widgets/windowingwidget.h
@@ -7,40 +7,6 @@ namespace Ui {
 class WindowingWidget;
 }
 
-class WindowingWidget : public QWidget
-{
-    Q_OBJECT
-
-public:
-    explicit WindowingWidget(QWidget *parent = nullptr);
-    ~WindowingWidget();
-
-    QPair<double, double> windowFromTo() const;
-    QPair<double, double> windowCenterWidth() const;
-
-    void setWindowFromTo(const QPair<double, double>& window);
-    void setWindowCenterWidth(const QPair<double, double> &window);
-
-signals:
-    void windowingChanged();
-    void autoWindowingRequested();
-
-private:
-    Ui::WindowingWidget *ui;
-
-    void updateFromTo();
-    void updateCenterWidth();
-
-private slots:
-    void fromChanged();
-    void toChanged();
-    void centerChanged();
-    void widthChanged();
-
-    void checkFromValid();
-    void checkToValid();
-};
-
 namespace CTL {
 namespace gui {
 
