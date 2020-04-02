53804f8bff07b71f6507ac922a77fe9252c176b6
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-04-02 14:15:09 +0200

    add a generalized plot() method

--- modules/src/gui/widgets/allwidgets.h
+++ modules/src/gui/widgets/allwidgets.h
@@ -18,4 +18,43 @@
 #include "gui/widgets/windowingwidget.h"
 #include "gui/widgets/zoomcontrolwidget.h"
 
+namespace CTL {
+namespace gui {
+namespace assist {
+
+inline void plot(AcquisitionSetup setup, uint maxNbViews = 100, bool sourceOnly = false,
+                 float visualScale = 50.0f)
+{ AcquisitionSetupView::plot(std::move(setup), maxNbViews, sourceOnly, visualScale); }
+
+inline void plot(Chunk2D<float> data,
+                 QPair<double,double> windowing = qMakePair(0.0, 0.0), double zoom = 1.0)
+{ Chunk2DView::plot(std::move(data), windowing, zoom); }
+
+inline void plot(CompositeVolume data)
+{ VolumeViewer::plot(std::move(data)); }
+
+inline void plot(ProjectionData projections, const ModuleLayout& layout = {})
+{ ProjectionViewer::plot(std::move(projections), layout); }
+
+inline void plot(SimpleCTsystem system, float visualScale = 50.0f)
+{ CTSystemView::plot(std::move(system), visualScale); }
+
+inline void plot(SpectralVolumeData data)
+{ VolumeViewer::plot(std::move(data)); }
+
+inline void plot(std::shared_ptr<AbstractDataModel> model,
+                 const QString& labelX = "x", const QString& labelY = "y")
+{ DataModelViewer::plot(std::move(model),labelX, labelY); }
+
+inline void plot(const IntervalDataSeries& intervalSeries, const QString& labelX = "x",
+                 const QString& labelY = "y", bool logAxisY = false)
+{ IntervalSeriesView::plot(intervalSeries, labelX, labelY, logAxisY); }
+
+inline void plot(const XYDataSeries& lineSeries, const QString& labelX = "x",
+                 const QString& labelY = "y", bool logAxisY = false)
+{ LineSeriesView::plot(lineSeries, labelX, labelY, logAxisY); }
+
+}
+}
+}
 #endif // CTL_ALLWIDGETS_H
