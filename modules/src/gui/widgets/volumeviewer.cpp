3b33312bb176e7e5ec1501b846d1ceca479f6d68
Tim Pfeiffer <tim.pfeiffer@ovgu.de>
2020-04-03 17:29:06 +0200

    doc VolumeViewer

--- modules/src/gui/widgets/volumeviewer.cpp
+++ modules/src/gui/widgets/volumeviewer.cpp
@@ -17,7 +17,12 @@ static const QVector<QPair<QString, QPair<double, double>>> WINDOW_PRESETS {
     qMakePair(QStringLiteral("Lungs"),   qMakePair(-1150.0,  350.0))
 };
 
-
+/*!
+ * Creates a VolumeViewer object with \a parent as a parent widget. Note that you need to call
+ * show() to display the window.
+ *
+ * The static method plot() can be used as a convenience alternative for quick visualization.
+ */
 VolumeViewer::VolumeViewer(QWidget* parent)
     : QWidget(parent)
     , ui(new Ui::VolumeViewer)
@@ -52,34 +57,34 @@ VolumeViewer::VolumeViewer(QWidget* parent)
     setWindowTitle("Volume Viewer");
 }
 
+/*!
+ * Creates a VolumeViewer with parent widget \a parent and sets its data to \a volume. Note that
+ * you need to call show() to display the window.
+ *
+ * The static method plot() can be used as a convenience alternative for quick visualization.
+ */
 VolumeViewer::VolumeViewer(CompositeVolume volume, QWidget *parent)
     : VolumeViewer(parent)
 {
     setData(std::move(volume));
 }
 
+/*!
+ * Deletes the object.
+ */
 VolumeViewer::~VolumeViewer()
 {
     delete ui;
 }
 
-void VolumeViewer::setData(SpectralVolumeData data)
-{
-    _compData = CompositeVolume(std::move(data));
-
-    updateVolumeOverview();
-    ui->_TW_volumeOverview->selectRow(0);
-}
-
-void VolumeViewer::setData(CompositeVolume data)
-{
-    _compData = std::move(data);
-
-    updateVolumeOverview();
-    if(_compData.nbSubVolumes())
-        ui->_TW_volumeOverview->selectRow(0);
-}
-
+/*!
+ * Creates a VolumeViewer to visualize \a data and shows the window.
+ *
+ * Sensitivity of windowing using mouse gestures is adapted automatically to \a data (see
+ * setAutoMouseWindowScaling()).
+ *
+ * The widget will be deleted automatically if the window is closed.
+ */
 void VolumeViewer::plot(CompositeVolume data)
 {
     auto viewer = new VolumeViewer(std::move(data));
@@ -90,26 +95,131 @@ void VolumeViewer::plot(CompositeVolume data)
     viewer->show();
 }
 
+/*!
+ * Creates a VolumeViewer to visualize \a data and shows the window.
+ *
+ * Sensitivity of windowing using mouse gestures is adapted automatically to \a data (see
+ * setAutoMouseWindowScaling()).
+ *
+ * The widget will be deleted automatically if the window is closed.
+ */
 void VolumeViewer::plot(SpectralVolumeData data)
 {
     plot(CompositeVolume(std::move(data)));
 }
 
+/*!
+ * Returns a (constant) reference to the data currently managed by this instance.
+ */
 const CompositeVolume& VolumeViewer::data() const { return _compData; }
 
+/*!
+ * Returns the viewport for displaying the actual slice data. Use this to configure the
+ * specific settings of the viewport.
+ *
+ * \sa Chunk2DView.
+ */
 Chunk2DView* VolumeViewer::dataView() const { return ui->_W_dataView; }
 
+/*!
+ * Sets the visualized data to \a data. Data is copied, so consider moving if it is no longer
+ * required.
+ *
+ * Applies a min/max windowing if no specific windowing has been set (ie. the current window is
+ * [0,0]).
+ */
+void VolumeViewer::setData(SpectralVolumeData data)
+{
+    setData(CompositeVolume(std::move(data)));
+}
+
+/*!
+ * Sets the visualized data to \a data. Data is copied, so consider moving if it is no longer
+ * required.
+ *
+ * By default, always selects the first subvolume in \a data for visualization.
+ *
+ * Applies a min/max windowing if no specific windowing has been set (ie. the current window is
+ * [0,0]).
+ */
+void VolumeViewer::setData(CompositeVolume data)
+{
+    _compData = std::move(data);
+
+    updateVolumeOverview();
+    if(_compData.nbSubVolumes())
+        ui->_TW_volumeOverview->selectRow(0);
+}
+
+/*!
+ * Sets the presets of the two preset buttons in the windowing GUI block to \a preset1 and
+ * \a preset2. Presets must contain the text that shall be shown on the button and the pair of
+ * values, specifying start and end of the data window, as a QPair.
+ *
+ * The window range will be shown as a tooltip when hovering the cursor over the corresponding
+ * button.
+ *
+ * Example:
+ * \code
+ * auto viewer = new VolumeViewer;
+ * // ...
+ *
+ * //                         Button label      Window: ( start,  end  )
+ * auto myPreset1 = qMakePair(QString("Soft"), qMakePair(-100.0,  200.0));
+ * auto myPreset2 = qMakePair(QString("Bone"), qMakePair(   0.0, 1200.0));
+ *
+ * viewer->setWindowPresets(myPreset1, myPreset2);
+ * viewer->show();
+ * \endcode
+ */
 void VolumeViewer::setWindowPresets(QPair<QString, QPair<double, double> > preset1,
                                     QPair<QString, QPair<double, double> > preset2)
 {
     ui->_W_windowing->setPresets(preset1, preset2);
 }
 
+/*!
+ * Sets the presets of the two preset buttons in the windowing GUI block to \a preset1 and
+ * \a preset2. Presets can be chosen from a set of pre-defined window ranges, specified in
+ * Hounsfield units (HU) [from, to]:
+ * - WindowPreset::Abdomen: [ -140.0,  260.0]
+ * - WindowPreset::Angio:   [    0.0,  600.0]
+ * - WindowPreset::Bone:    [ -450.0, 1050.0]
+ * - WindowPreset::Brain:   [    0.0,   80.0]
+ * - WindowPreset::Chest:   [ -160.0,  240.0]
+ * - WindowPreset::Lungs:   [-1150.0,  350.0]
+ *
+ * Example:
+ * \code
+ * auto viewer = new VolumeViewer;
+ * // ...
+ *
+ * viewer->setWindowPresets(WindowPreset::Abdomen, WindowPreset::Lungs);
+ * viewer->show();
+ * \endcode
+ */
 void VolumeViewer::setWindowPresets(WindowPreset preset1, WindowPreset preset2)
 {
     setWindowPresets(WINDOW_PRESETS.at(int(preset1)), WINDOW_PRESETS.at(int(preset2)));
 }
 
+/*!
+ * Sets the presets of the two preset buttons in the windowing GUI block to \a preset1 and
+ * \a preset2. Presets can be chosen from a set of pre-defined window ranges (see
+ * setWindowPresets(WindowPreset, WindowPreset)). Window ranges from the presets (defined in
+ * Hounsfield units) will be converted to attenuation coefficients with respect to the reference
+ * energy \a referenceEnergy.
+ *
+ * Example:
+ * \code
+ * auto viewer = new VolumeViewer;
+ * // ...
+ *
+ * // set Abdomen and Lung presets for attenuation coefficients with reference energy 75 keV
+ * viewer->setWindowPresets(WindowPreset::Abdomen, WindowPreset::Lungs, 75.0f);
+ * viewer->show();
+ * \endcode
+ */
 void VolumeViewer::setWindowPresetsInMu(WindowPreset preset1, WindowPreset preset2,
                                         float referenceEnergy)
 {
@@ -129,8 +239,8 @@ void VolumeViewer::setWindowPresetsInMu(WindowPreset preset1, WindowPreset prese
 
 /*!
  * Requests an automatic resizing of this widget's window size. The window is tried to fit to the
- * size of the shown data, bounded between a minimum size of 800 x 400 pixels and a maximum size of
- * 1300 x 900 pixels.
+ * size of the shown data, bounded between a minimum size of 700 x 400 pixels (500 x 400 with
+ * hidden composite overview) and a maximum size of 1300 x 900 pixels.
  */
 void VolumeViewer::autoResize()
 {
@@ -139,7 +249,7 @@ void VolumeViewer::autoResize()
 
     QSize minimumSize = staticMinSize;
     QSize totalMargin = staticMargin;
-    if(!ui->_TW_volumeOverview->isHidden())
+    if(!ui->_TW_volumeOverview->isHidden()) // the overview is shown and needs extra space
     {
         totalMargin.rwidth() += (ui->_TW_volumeOverview->width() - 14);
         minimumSize.rwidth() += 200;
@@ -157,11 +267,22 @@ void VolumeViewer::hideCompositeOverview(bool hide)
     ui->_TW_volumeOverview->setVisible(!hide);
 }
 
+/*!
+ * Convenience method to set automatically determined values for the sensitvity of windowing using
+ * mouse gestures.
+ *
+ * Same as: \code dataView()->setAutoMouseWindowScaling(); \endcode
+ *
+ * \sa Chunk2DView::setAutoMouseWindowScaling().
+ */
 void VolumeViewer::setAutoMouseWindowScaling()
 {
     ui->_W_dataView->setAutoMouseWindowScaling();
 }
 
+/*!
+ * Shows the slice with index \a slice in the currently selected slice direction.
+ */
 void VolumeViewer::showSlice(int slice)
 {
     ui->_L_slice->setNum(slice);
@@ -171,9 +292,11 @@ void VolumeViewer::showSlice(int slice)
         ui->_W_dataView->setData(selectedVolume().sliceY(static_cast<uint>(slice)));
     else if(ui->_RB_directionZ->isChecked())
         ui->_W_dataView->setData(selectedVolume().sliceZ(static_cast<uint>(slice)));
-
 }
 
+/*!
+ * Shows the subvolume with index \a subvolume from the currently managed dataset.
+ */
 void VolumeViewer::showSubvolume(int subvolume)
 {
     ui->_TW_volumeOverview->selectRow(subvolume);
--- modules/src/gui/widgets/volumeviewer.h
+++ modules/src/gui/widgets/volumeviewer.h
@@ -13,6 +13,97 @@ namespace gui {
 
 class Chunk2DView;
 
+/*!
+ * \class VolumeViewer
+ *
+ * \brief The VolumeViewer class provides a tool for visualization of volume data.
+ *
+ * This class can be used to visualize different volume data objects. It fully supports:
+ * - VoxelVolume<float>
+ * - SpectralVolumeData
+ * - CompositeVolume
+ * Any subclass of AbstractDynamicVolumeData can be visualized as well, but will be limited to
+ * displaying the current state of the volume (ie. as set by AbstractDynamicVolumeData::setTime()).
+ *
+ * For convenience, the plot() method can be used to achieve a one-line solution, creating a
+ * widget that will be destroyed once it is closed by the user.
+ *
+ * ![A VolumeViewer window visualizing data of a CompositeVolume containing two subvolumes: a water cylinder (currently shown) a ball.](gui/VolumeViewer.png)
+ *
+ * Use the vertical slider to cylce through the slices of the current volume. When visualizing a
+ * CompositeVolume, the table widget on the right shows an overview over its different subvolumes.
+ * Click on a row to visualize the corresponding subvolume in the viewport. If desired (e.g. to save
+ * screen space), the subvolume overview can be hidden with hideCompositeOverview). Subvolumes can
+ * then only be selected using showSubvolume().
+ *
+ * The following IO operations are supported by this widget:
+ *
+ * Within the viewport of the current slice:
+ * - Zooming:
+ *    - Hold CRTL + scroll mouse wheel up/down to zoom in/out.
+ * - Data windowing:
+ *    - Hold left mouse button + move up/down to raise/lower the center (or level) of the window.
+ *    - Hold left mouse button + move left/right to narrow/broaden the width of the window.
+ *    - Double-click left to request automatic windowing (ie. min/max-window).
+ * - Plotting a contrast line:
+ *    - Hold right mouse button + drag mouse to draw a line.
+ *    - Press 'K' button to create a contrast line plot of the current line (requires
+ * ctl_gui_charts.pri submodule).
+ * - Read-out live pixel data under cursor:
+ *    - Mouse movements: Live pixel data is shown under the bottom right corner of the image.
+ *
+ * Anywhere in the widget:
+ * - Save to image:
+ *    - Press CRTL + S to open a dialog for saving the current figure to a file.
+ * - Plotting a contrast line:
+ *    - Press 'K' button to create a contrast line plot of the current line (requires
+ * ctl_gui_charts.pri submodule).
+ *
+ * Contrast line plots are opened in separate windows (see LineSeriesView for details on the
+ * corresponding widget).
+ *
+ * Visualization of the slice image itself is done using the Chunk2DView class. The viewport can be
+ * accessed with dataView(), in order to adjust its settings. For mouse gesture windowing, a
+ * convenience method setAutoMouseWindowScaling() exists to directly set a sensitivity suited
+ * for the current data.
+ *
+ * Example:
+ * \code
+ * // Creating two volumes
+ * auto cylinderVol = SpectralVolumeData::cylinderX(30.0f, 100.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Water));
+ * auto ballVolume  = SpectralVolumeData::ball(10.0f, 1.0f, 1.0f, attenuationModel(database::Composite::Blood));
+ *
+ * // visualize a single volume (static version)
+ * gui::VolumeViewer::plot(cylinderVol);
+ *
+ * //create a CompositeVolume and visualize it (property-based version)
+ * CompositeVolume compositeVol(std::move(cylinderVol), std::move(ballVolume));
+ *
+ * auto viewer = new gui::VolumeViewer; // needs to be deleted at an appropriate time
+ * viewer->setData(compositeVol);
+ * viewer->autoResize();
+ * viewer->show();
+ * \endcode
+ *
+ * Note that it is absolutely possible to use the static version also to visualize a
+ * CompositeVolume. However, he property-based version provides more flexibility as it gives access
+ * to the full set of methods to configure the VolumeViewer. In particular, it allows us to
+ * configure the viewport showing the individual slices. The following example shows, how we can use
+ * this approach to create a visualization that uses a black-red colormap:
+ * \code
+ * // create colormap
+ * QVector<QRgb> blackRedMap(256);
+ *  for(int i = 0; i <= 255; ++i)
+ *      blackRedMap[i] = qRgb(i,0,0);
+ *
+ * auto viewer = new gui::VolumeViewer; // needs to be deleted at an appropriate time
+ * viewer->setData(compositeVol);       // see example above for 'compositeVol'
+ * viewer->dataView()->setColorTable(blackRedMap);
+ *
+ * viewer->autoResize();
+ * viewer->show();
+ * \endcode
+ */
 class VolumeViewer : public QWidget
 {
     Q_OBJECT
