e4e73dd78cb92d99bfeea97f526de268e9c394ce
frysch <robert.frysch@ovgu.de>
2020-04-29 09:31:03 +0200

    complete header consistency script for all CTL modules

--- modules/.check_header_consistency.sh
+++ modules/.check_header_consistency.sh
@@ -41,3 +41,36 @@ then
 	echo 'ERROR: Number of header in ctl.h / ctl.pri: ' $NB_LINES_HEADER ' / ' $NB_LINES_MODULE
 	exit 1
 fi
+
+# CTL OpenCL module
+NB_LINES_HEADER=$(grep -c '^#include "' src/ctl_ocl.h)
+NB_LINES_MODULE=$(cat submodules/ocl_config.pri submodules/ocl_routines.pri |
+				  grep -Ec '\s+\$\$PWD/.*\.h( \\)?\s?$')
+
+if [ $NB_LINES_HEADER -ne $NB_LINES_MODULE ]
+then
+	echo 'ERROR: Number of header in ctl_ocl.h / ctl_ocl.pri: ' $NB_LINES_HEADER ' / ' $NB_LINES_MODULE
+	exit 1
+fi
+
+# CTL QtGUI module
+NB_LINES_HEADER=$(grep -c '^#include "' src/ctl_qtgui.h)
+NB_LINES_MODULE=$(cat submodules/gui_widgets.pri submodules/gui_widgets_3d.pri submodules/gui_widgets_charts.pri submodules/gui_widgets_ocl.pri |
+				  grep -Ec '\s+\$\$PWD/.*\.h( \\)?\s?$')
+
+if [ $NB_LINES_HEADER -ne $NB_LINES_MODULE ]
+then
+	echo 'ERROR: Number of header in ctl_qtgui.h / ctl_qtgui.pri: ' $NB_LINES_HEADER ' / ' $NB_LINES_MODULE
+	exit 1
+fi
+
+# CTL NLopt module
+NB_LINES_HEADER=$(grep -c '^#include "' src/ctl_nlopt.h)
+NB_LINES_MODULE=$(cat submodules/grangeat_2d3d_regist.pri |
+				  grep -Ec '\s+\$\$PWD/.*\.h( \\)?\s?$')
+
+if [ $NB_LINES_HEADER -ne $NB_LINES_MODULE ]
+then
+	echo 'ERROR: Number of header in ctl_nlopt.h / ctl_nlopt.pri: ' $NB_LINES_HEADER ' / ' $NB_LINES_MODULE
+	exit 1
+fi
